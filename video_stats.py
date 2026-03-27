5bc633311f8d83b0bf457f8771701d0fbbf65797
usukhbayar.s <usukhbayar.s@corp.unitel.mn>
2026-03-27 08:52:46 +0800

    api updated

--- video_stats.py
+++ video_stats.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import os
 
-API_KEY = 'AIzaSyB9j1fn4QvdidfRlHpyg8i0O7Fq9RApXxs'
-channel_handle = 'MrBeast'
-url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={API_KEY}'
+API_KEY = os.getenv('API_KEY')
+CHANNEL_HANDLE = os.getenv('CHANNEL_HANDLE')
+
+url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
 
 response = requests.get(url)
 print(response)
