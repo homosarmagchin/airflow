0d29314ac74fbaf25b28ab534b76e16ead63d93f
usukhbayar.s <usukhbayar.s@corp.unitel.mn>
2026-03-31 23:19:25 +0800

    get video id

--- video_stats.py
+++ video_stats.py
@@ -6,6 +6,8 @@ load_dotenv(dotenv_path='.env')
 
 API_KEY = os.getenv('API_KEY')
 CHANNEL_HANDLE = os.getenv('CHANNEL_HANDLE')
+maxResults = 50
+
 
 def get_playlist_id():
     try:
@@ -26,7 +28,40 @@ def get_playlist_id():
         print(f"An error occurred: {e}")
         return None
     
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails,snippet&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    
+    try:
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            print(f"Fetched {len(data.get('items', []))} items from playlist.")
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+    
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None    
+    
+
 if __name__ == "__main__":
-    get_playlist_id()
-else:
-    print("This script is intended to be run directly.")
+    playlistId = get_playlist_id()
+    video_ids = get_video_ids(playlistId)
+    print(video_ids)
