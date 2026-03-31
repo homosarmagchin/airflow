c0dac6a50a503208082a631d720ed37eedd88228
usukhbayar.s <usukhbayar.s@corp.unitel.mn>
2026-03-31 22:30:11 +0800

    updated scripts

--- video_stats.py
+++ video_stats.py
@@ -2,16 +2,33 @@ import requests
 import json
 import os
 
-API_KEY = os.getenv('API_KEY')
-CHANNEL_HANDLE = os.getenv('CHANNEL_HANDLE')
+# API_KEY = os.getenv('API_KEY')
+# CHANNEL_HANDLE = os.getenv('CHANNEL_HANDLE')
 
-url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
-
-response = requests.get(url)
-print(response)
-
-data = response.json()
-
-print(json.dumps(data, indent=4))
+API_KEY = 'AIzaSyB9j1fn4QvdidfRlHpyg8i0O7Fq9RApXxs'
+CHANNEL_HANDLE = 'MrBeast'
 
 
+def get_playlist_id():
+    try:
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+
+        response = requests.get(url)
+        response.raise_for_status()  
+
+        data = response.json()
+        channel_items = data['items'][0]
+        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        
+        print(f"Channel Playlist ID: {channel_playlistId}")
+
+        return channel_playlistId
+    
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+if __name__ == "__main__":
+    get_playlist_id()
+else:
+    print("This script is intended to be run directly.")
